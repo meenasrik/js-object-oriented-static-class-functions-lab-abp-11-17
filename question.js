db7d8071fe8895587fae25fa63bd15de61aa0fad
Meenakshi Radhakrishnan <junnamma@yahoo.com>
2017-11-13 16:47:26 +0000

    Done.

--- question.js
+++ question.js
@@ -1,1 +1,22 @@
 'use strict';
+class Question {
+
+  constructor (content) {
+    this.content = content;
+    this.save();
+  }
+
+  save() {
+    Question._All.push(this);
+  }
+
+  static All() {
+    return this._All;
+  }
+
+  static Find(id) {
+    return this._All[id - 1];
+  }
+}
+
+Question._All = [];
